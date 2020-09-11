792efe7c2e709a38e1787bd52f93c8c9857ca9cb
polubis <polubis@wp.pl>
2020-09-11 13:24:29 +0200

    Init

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
+
+import App from './App';
+
+import 'styles/index.scss';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat:300,400,500,700', 'sans-serif']
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,140 @@
+const path = require('path');
+
+const DefinePlugin = require('webpack').DefinePlugin;
+const TsConfigPathsPlugin = require('awesome-typescript-loader').TsConfigPathsPlugin;
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, { mode }) => {
+  const [DEV, PROD] = ['development', 'production'];
+
+  console.log(`App is running in ${mode} mode`);
+
+  const config = {
+    mode,
+
+    devtool: mode === PROD ? false : 'inline-source-map',
+
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        styles: path.resolve(__dirname, 'src/styles')
+      },
+      plugins: [new TsConfigPathsPlugin()]
+    },
+
+    output: {
+      path: __dirname + '/dist',
+      filename: '[name].[hash].js',
+      publicPath: '/'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'awesome-typescript-loader'
+        },
+        {
+          test: /\.scss$/,
+          exclude: path.resolve(__dirname, 'src/styles/'),
+          use: [
+            'style-loader',
+            'css-modules-typescript-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                modules: {
+                  localIdentName: '[local]___[hash:base64:5]'
+                }
+              }
+            },
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          include: path.resolve(__dirname, 'src/styles/'),
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+          exclude: /node_modules/,
+          use: ['file-loader?name=[name].[ext]']
+        },
+        {
+          test: /\.(config)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        favicon: './public/favicon.ico',
+        inject: 'body',
+        hash: true
+      }),
+      new InterpolateHtmlPlugin({
+        PUBLIC_URL: 'public'
+      }),
+      new DefinePlugin({})
+    ],
+
+    devServer: {
+      historyApiFallback: true,
+      port: 3000
+    }
+  };
+
+  if (mode === PROD) {
+    config.optimization = {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        cacheGroups: {
+          default: false, // Removes default config
+
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // We creating here node_modules single package name
+              return `npm.${module.context
+                .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                .replace('@', '')}`;
+            },
+            minSize: 0
+          }
+        }
+      }
+    };
+
+    config.plugins.push(new CopyPlugin([{ from: 'public', ignore: ['index.html'] }]));
+  }
+
+  if (mode !== PROD) {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      })
+    );
+  }
+
+  return config;
+};
